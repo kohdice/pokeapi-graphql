13fad1097380c0481bbbc98743f5c01b92fbaf8e
kohdice <kohdice@gmail.com>
2024-02-10 21:22:32 +0900

    feat: Add PokemonAbilities model

---
+++ pokeapi/infrastructure/db/models/pokemon_abilities.py
@@ -0,0 +1,24 @@
+from sqlalchemy import Boolean, ForeignKey, SmallInteger
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from .base import Base
+from .mixins import TimestampMixin
+
+
+class PokemonAbilities(Base, TimestampMixin):
+    """Class that maps to the `pokemon_abilities` table."""
+
+    __tablename__ = "pokemon_abilities"
+
+    pokemon_id: Mapped[int] = mapped_column(
+        ForeignKey("pokemon_mst.id"), primary_key=True
+    )
+    ability_id: Mapped[int] = mapped_column(
+        ForeignKey("ability_mst.id"), primary_key=True
+    )
+    slot: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    is_hidden: Mapped[bool] = mapped_column(Boolean, nullable=False)
+
+    # Relationships
+    pokemon = relationship("PokemonMst", back_populates="pokemon_abilities")
+    ability = relationship("AbilityMst", back_populates="pokemon_abilities")
