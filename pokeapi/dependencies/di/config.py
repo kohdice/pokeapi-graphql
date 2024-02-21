c9286ce7e7f5987084a47ebd4b9e429203cc0299
kohdice <kohdice@gmail.com>
2024-02-21 21:45:36 +0900

    feat: Add ConfigModule

---
+++ pokeapi/dependencies/di/config.py
@@ -0,0 +1,16 @@
+from injector import Binder, Module, singleton
+
+from pokeapi.dependencies.settings import AppConfig, AppConfigABC
+
+
+class ConfigModule(Module):
+    """The configuration module.
+
+    This class is responsible for providing the configuration to the application.
+
+    """
+
+    def configure(self, binder: Binder) -> None:
+        """Configure the module."""
+
+        binder.bind(AppConfigABC, to=AppConfig, scope=singleton)  # type: ignore[type-abstract]
