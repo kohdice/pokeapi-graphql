7e8c3b7b0bf58c153d393d88b7fa2cdbccd4cefa
kohdice <kohdice@gmail.com>
2024-02-21 21:07:09 +0900

    refactor: Fix argument of session_factory to accept AppConfig

--- pokeapi/infrastructure/database/db.py
+++ pokeapi/infrastructure/database/db.py
@@ -3,21 +3,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
 from pokeapi.infrastructure.database.utils import adjust_connection_url
-from pokeapi.settings.config_abc import ConnectionUrl
+from pokeapi.settings.config import AppConfig
 
 
 @singleton
 @provider
-def session_factory(connection_url: ConnectionUrl) -> sessionmaker[Session]:
+def session_factory(config: AppConfig) -> sessionmaker[Session]:
     """Create a new session factory.
 
     Args:
-        connection_url (ConnectionUrl): The connection URL.
+        config (AppConfig): The application configuration.
 
     Returns:
         sessionmaker: The session factory.
 
     """
-    engine = create_engine(adjust_connection_url(connection_url), echo=True)
+    engine = create_engine(
+        adjust_connection_url(config.database_url), echo=config.debug
+    )
 
     return sessionmaker(bind=engine, autocommit=False, autoflush=False)
