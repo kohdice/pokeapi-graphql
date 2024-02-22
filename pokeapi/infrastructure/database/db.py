498cf0227f64c516600744f5e413fb1b1b75090b
kohdice <kohdice@gmail.com>
2024-02-22 21:23:46 +0900

    refactor: Fix return value of session_factory to Session

--- pokeapi/infrastructure/database/db.py
+++ pokeapi/infrastructure/database/db.py
@@ -1,14 +1,15 @@
-from injector import provider, singleton
+from injector import inject, provider, singleton
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 
-from pokeapi.dependencies.settings import AppConfig
+from pokeapi.dependencies.settings.config import AppConfig
 from pokeapi.infrastructure.database.utils import adjust_connection_url
 
 
 @singleton
+@inject
 @provider
-def session_factory(config: AppConfig) -> sessionmaker[Session]:
+def session_factory(config: AppConfig) -> Session:
     """Create a new session factory.
 
     Args:
@@ -21,5 +22,7 @@ def session_factory(config: AppConfig) -> sessionmaker[Session]:
     engine = create_engine(
         adjust_connection_url(config.database_url), echo=config.debug
     )
+    session_local = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
-    return sessionmaker(bind=engine, autocommit=False, autoflush=False)
+    with session_local() as session:
+        return session
