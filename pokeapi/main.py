e16af9597be7064f0b3c7cd7604894756adcaade
kohdice <kohdice@gmail.com>
2024-02-24 23:12:09 +0900

    feat: Set get_context in GraphQLRouter

--- pokeapi/main.py
+++ pokeapi/main.py
@@ -2,13 +2,16 @@ import strawberry
 from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 
+from pokeapi.dependencies.context import get_context
 from pokeapi.presentation.schemas.query import Query
 
 schema = strawberry.Schema(Query)
-graphql_app: GraphQLRouter = GraphQLRouter(schema)
+graphql_app: GraphQLRouter = GraphQLRouter(
+    schema, context_getter=get_context, path="/graphql"
+)
 
 app = FastAPI()
-app.include_router(graphql_app, prefix="/graphql")
+app.include_router(graphql_app)
 
 
 @app.get("/health")
