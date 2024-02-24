37dcedcd4682997f1f9407854a6f6a660042a089
kohdice <kohdice@gmail.com>
2024-02-24 23:03:19 +0900

    fix: Fixed A and B to not inherit from relay.Node

--- pokeapi/presentation/schemas/pokemons_type.py
+++ pokeapi/presentation/schemas/pokemons_type.py
@@ -1,12 +1,10 @@
 import strawberry
-from strawberry import relay
-from strawberry.types import Info
 
 from pokeapi.presentation.schemas.pokemon_type import PokemonType
 
 
 @strawberry.type
-class PokemonsType(relay.Node):
+class PokemonsType:
     """GraphQL schema for Pokémon's Type.
 
     Attributes:
@@ -15,28 +13,7 @@ class PokemonsType(relay.Node):
 
     """
 
-    id: relay.NodeID[str]
     pokemon_type: PokemonType = strawberry.field(
         description="Ability of this Pokémon's Type"
     )
     slot: int = strawberry.field(description="Slot of this Pokémon's type")
-
-    # TODO: Enable returning data retrieved from the database using the node_id.
-    @classmethod
-    def resolve_node(
-        cls, node_id: str, *, info: Info | None = None, required: bool = False
-    ) -> "PokemonsType":
-        """Resolve Pokémon's Type.
-
-        Args:
-            node_id (str): The unique identifier for the Pokémon's Type.
-            info (Info, optional): Information about the execution of the query.
-            required (bool, optional): Whether the node is required.
-
-        Returns:
-            PokemonsType: The Pokémon's Type.
-
-        """
-        return PokemonsType(
-            id=node_id, pokemon_type=PokemonType(id=1, type_name="ノーマル"), slot=1
-        )
