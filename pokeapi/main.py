215615966c5e4eb07ba2d0baea1b9a32fb7e75fc
kohdice <kohdice@gmail.com>
2024-02-25 22:11:57 +0900

    feat: Set up logger using configure_logging in main.py

--- pokeapi/main.py
+++ pokeapi/main.py
@@ -3,7 +3,10 @@ from fastapi import FastAPI
 from strawberry.fastapi import GraphQLRouter
 
 from pokeapi.dependencies.context import get_context
+from pokeapi.infrastructure.logger import configure_logging
 from pokeapi.presentation.schemas.query import Query
+
+configure_logging()
 
 schema = strawberry.Schema(Query)
 graphql_app: GraphQLRouter = GraphQLRouter(
