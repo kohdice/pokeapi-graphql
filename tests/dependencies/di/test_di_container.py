96346d82bd8d1c3d1f7206cecc6ce94510c449c1
kohdice <kohdice@gmail.com>
2024-02-25 16:41:54 +0900

    test: Add tests for the DI container

--- tests/dependencies/di/test_di_container.py
+++ tests/dependencies/di/test_di_container.py
@@ -3,9 +3,11 @@ from injector import Injector
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
+from pokeapi.application.services.pokemon import PokemonService
 from pokeapi.dependencies.di.config import ConfigModule
 from pokeapi.dependencies.di.database import DatabaseModule
 from pokeapi.dependencies.di.repository import RepositoryModule
+from pokeapi.dependencies.di.service import ServiceModule
 from pokeapi.dependencies.settings import AppConfig
 from pokeapi.domain.entities.pokemon import Pokemon as PokemonEntity
 from pokeapi.infrastructure.database.models import Pokemon as PokemonModel
@@ -13,9 +15,11 @@ from pokeapi.infrastructure.database.repositories.pokemon import PokemonReposito
 
 
 class TestDIContainer:
-    @pytest.fixture()
+    @pytest.fixture(scope="module")
     def dependency_container(self) -> Injector:
-        return Injector([ConfigModule(), DatabaseModule(), RepositoryModule()])
+        return Injector(
+            [ConfigModule(), DatabaseModule(), RepositoryModule(), ServiceModule()]
+        )
 
     def test_singleton(self, dependency_container: Injector) -> None:
         session_1 = dependency_container.get(Session)
@@ -32,6 +36,11 @@ class TestDIContainer:
         repo_2 = dependency_container.get(PokemonRepository)
 
         assert repo_1 is repo_2
+
+        service_1 = dependency_container.get(PokemonService)
+        service_2 = dependency_container.get(PokemonService)
+
+        assert service_1 is service_2
 
     def test_di_config(self, dependency_container: Injector) -> None:
         actual = dependency_container.get(AppConfig)
@@ -51,6 +60,13 @@ class TestDIContainer:
     def test_di_pokemon_repo(self, dependency_container: Injector) -> None:
         repo = dependency_container.get(PokemonRepository)
         actual_1 = repo.get_by_id(1)
+
+        assert isinstance(actual_1, PokemonEntity)
+        assert actual_1.name == "フシギダネ"
+
+    def test_di_pokemon_service(self, dependency_container: Injector) -> None:
+        service = dependency_container.get(PokemonService)
+        actual_1 = service.get_by_id(1)
 
         assert isinstance(actual_1, PokemonEntity)
         assert actual_1.name == "フシギダネ"
