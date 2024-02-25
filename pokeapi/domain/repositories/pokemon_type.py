f02c26f1193d399a93d98a3c5861e5aa02b5d951
kohdice <kohdice@gmail.com>
2024-02-25 15:46:13 +0900

    feat: Add TypeRepositoryABC

---
+++ pokeapi/domain/repositories/pokemon_type.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+
+from pokeapi.domain.entities.base import BaseEntity
+from pokeapi.infrastructure.database.models.base import BaseModel
+
+
+class TypeRepositoryABC(ABC):
+    """Abstract base class for sample repositories.
+
+    This class defines the interface for pokemon repositories. Concrete implementations
+    should inherit from this class and provide implementations for the abstract methods.
+
+    """
+
+    @abstractmethod
+    def _convert_to_entity(self, model: BaseModel) -> BaseEntity:
+        """Converts a SQLAlchemy model to a domain entity.
+
+        This method converts a SQLAlchemy model instance to a corresponding domain entity
+        instance.
+
+        Args:
+            model (BaseModel): The SQLAlchemy model instance to be converted.
+
+        Returns:
+            BaseEntity: The converted instance of the domain entity.
+
+        """
+        pass  # pragma: no cover
+
+    @abstractmethod
+    def get_by_id(self, id_: int) -> BaseEntity | None:
+        """Retrieve an entity by its identifier.
+
+        Args:
+            id_ (int): The identifier of the entity to retrieve.
+
+        Returns:
+            BaseEntity | None: The entity with the specified identifier, or None if not found.
+
+        """
+        pass  # pragma: no cover
+
+    @abstractmethod
+    def get_all(self) -> list[BaseEntity]:
+        """Retrieve all entities from the repository.
+
+        Returns:
+            list[BaseEntity]: A list of all entities in the repository.
+
+        """
+        pass  # pragma: no cover
