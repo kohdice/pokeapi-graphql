1f61cb0594f8da79e3b15958a0fccf1aa090b368
kohdice <kohdice@gmail.com>
2024-03-02 20:40:59 +0900

    feat: Add timestamp fields to TokenWhitelist entity

--- pokeapi/domain/entities/token_whitelist.py
+++ pokeapi/domain/entities/token_whitelist.py
@@ -1,3 +1,5 @@
+import datetime
+
 from pokeapi.domain.entities.base import BaseEntity
 
 
@@ -9,9 +11,17 @@ class TokenWhitelist(BaseEntity):
         user_id (int): The user_id of the TokenWhitelist.
         access_token (str): The access_token of the TokenWhitelist.
         refresh_token (str): The refresh_token of the TokenWhitelist.
+        created_by (str): The user who created the TokenWhitelist.
+        created_at (datetime.datetime): The date and time the TokenWhitelist was created.
+        updated_by (str): The user who last updated the TokenWhitelist.
+        updated_at (datetime.datetime): The date and time the TokenWhitelist was last updated.
     """
 
     id_: int | None
     user_id: int
     access_token: str
     refresh_token: str
+    created_by: str | None
+    created_at: datetime.datetime | None
+    updated_by: str | None
+    updated_at: datetime.datetime | None
