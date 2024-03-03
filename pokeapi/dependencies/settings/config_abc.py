6d2a433af1708a30b23b21e9c5fef1faac09504c
kohdice <kohdice@gmail.com>
2024-03-03 21:18:07 +0900

    feat: Add properties to AppConfigABC

--- pokeapi/dependencies/settings/config_abc.py
+++ pokeapi/dependencies/settings/config_abc.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from functools import cache
 
 
 class AppConfigABC(ABC):
@@ -11,8 +12,37 @@ class AppConfigABC(ABC):
         stage: The stage of the application (e.g. 'development', 'staging', 'production').
         debug: A boolean indicating whether the application is in debug mode.
         database_url: The URL for the database connection.
+        private_key: The private key for the application.
+        public_key: The public key for the application.
+        jwt_algorithm: The algorithm to use for JWT signing.
+        access_token_lifetime: The lifetime of access tokens (in hours).
+        refresh_token_lifetime: The lifetime of refresh tokens (in hours).
 
     """
+
+    @staticmethod
+    @cache
+    @abstractmethod
+    def _load_private_key() -> bytes:
+        """Load the private key for the application.
+
+        Returns:
+            bytes: The private key for the application.
+
+        """
+        pass  # pragma: no cover
+
+    @staticmethod
+    @cache
+    @abstractmethod
+    def _load_public_key() -> bytes:
+        """Load the public key for the application.
+
+        Returns:
+            bytes: The public key for the application.
+
+        """
+        pass  # pragma: no cover
 
     @property
     @abstractmethod
@@ -43,6 +73,61 @@ class AppConfigABC(ABC):
 
         Returns:
             str: The URL for the database connection.
+
+        """
+        pass  # pragma: no cover
+
+    @property
+    @abstractmethod
+    def private_key(self) -> bytes:
+        """The private key for the application.
+
+        Returns:
+            bytes: The private key for the application.
+
+        """
+        pass  # pragma: no cover
+
+    @property
+    @abstractmethod
+    def public_key(self) -> bytes:
+        """The public key for the application.
+
+        Returns:
+            bytes: The public key for the application.
+
+        """
+        pass  # pragma: no cover
+
+    @property
+    @abstractmethod
+    def jwt_algorithm(self) -> str:
+        """The algorithm to use for JWT signing.
+
+        Returns:
+            str: The algorithm to use for JWT signing.
+
+        """
+        pass  # pragma: no cover
+
+    @property
+    @abstractmethod
+    def access_token_lifetime(self) -> int:
+        """The lifetime of access tokens (in hours).
+
+        Returns:
+            int: The lifetime of access tokens.
+
+        """
+        pass  # pragma: no cover
+
+    @property
+    @abstractmethod
+    def refresh_token_lifetime(self) -> int:
+        """The lifetime of refresh tokens (in hours).
+
+        Returns:
+            int: The lifetime of refresh tokens.
 
         """
         pass  # pragma: no cover
