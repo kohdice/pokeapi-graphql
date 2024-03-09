e414ca7b7ccc1bfacb09ccb003eadd784ced9604
kohdice <kohdice@gmail.com>
2024-03-10 00:05:13 +0900

    feat: Add UserNotFoundError

--- pokeapi/exceptions/user.py
+++ pokeapi/exceptions/user.py
@@ -2,5 +2,9 @@ class UserCreationError(Exception):
     """Raised when an error occurs while creating a user."""
 
 
+class UserNotFoundError(Exception):
+    """Raised when a user is not found."""
+
+
 class UserUpdateError(Exception):
     """Raised when an error occurs while updating a user."""
