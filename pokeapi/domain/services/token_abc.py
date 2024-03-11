7f5bd4cbd3364cacb2e7c93235c6991978ac49ae
kohdice <kohdice@gmail.com>
2024-03-11 22:21:02 +0900

    feat: Add TokenService

---
+++ pokeapi/domain/services/token_abc.py
@@ -0,0 +1,49 @@
+from abc import ABC, abstractmethod
+
+from pokeapi.domain.entities.token import Token
+from pokeapi.domain.entities.user import User
+
+
+class TokenServiceABC(ABC):
+    """Abstract class for token creation and management.
+
+    This class is an abstract class that defines the methods that a token
+    creation and management service should implement.
+
+    """
+
+    @abstractmethod
+    def create(self, entity: User) -> Token:
+        """Create a token for the given entity.
+
+        Args:
+            entity (User): The entity to be used as the payload.
+
+        Returns:
+            Token: The token issued to the entity.
+        """
+        pass  # pragma: no cover
+
+    @abstractmethod
+    def update(self, entity: User, whitelist_id: int) -> Token:
+        """Update a token for the given entity.
+
+        Args:
+            entity (User): The entity to be used as the payload.
+            whitelist_id (int): The ID of the token to be updated.
+
+        Returns:
+            Token: The token issued to the entity.
+
+        """
+        pass  # pragma: no cover
+
+    @abstractmethod
+    def delete(self, entity: User, whitelist_id: int) -> None:
+        """Delete a token for the given entity.
+
+        Args:
+            entity (User): The entity to be used as the payload.
+
+        """
+        pass  # pragma: no cover
