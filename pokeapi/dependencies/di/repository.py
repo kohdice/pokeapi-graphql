a7f1701afaea6bdd2fd362404269b9d63c9dd472
kohdice <kohdice@gmail.com>
2024-03-12 21:58:17 +0900

    feat: Add configuration for UserRepository to RepositoryModule

--- pokeapi/dependencies/di/repository.py
+++ pokeapi/dependencies/di/repository.py
@@ -3,11 +3,17 @@ from injector import Binder, Module, singleton
 from pokeapi.domain.repositories.pokemon import PokemonRepositoryABC
 from pokeapi.domain.repositories.pokemon_ability import AbilityRepositoryABC
 from pokeapi.domain.repositories.pokemon_type import TypeRepositoryABC
+from pokeapi.domain.repositories.token_whitelist import TokenWhitelistRepositoryABC
+from pokeapi.domain.repositories.user import UserRepositoryABC
 from pokeapi.infrastructure.database.repositories.pokemon import PokemonRepository
 from pokeapi.infrastructure.database.repositories.pokemon_ability import (
     AbilityRepository,
 )
 from pokeapi.infrastructure.database.repositories.pokemon_type import TypeRepository
+from pokeapi.infrastructure.database.repositories.token_whitelist import (
+    TokenWhitelistRepository,
+)
+from pokeapi.infrastructure.database.repositories.user import UserRepository
 
 
 class RepositoryModule(Module):
@@ -25,5 +31,11 @@ class RepositoryModule(Module):
 
         """
         binder.bind(PokemonRepositoryABC, to=PokemonRepository, scope=singleton)  # type: ignore[type-abstract]
-        binder.bind(TypeRepositoryABC, to=TypeRepository, scope=singleton)  # type: ignore[type-abstract]
         binder.bind(AbilityRepositoryABC, to=AbilityRepository, scope=singleton)  # type: ignore[type-abstract]
+        binder.bind(
+            TokenWhitelistRepositoryABC,  # type: ignore[type-abstract]
+            to=TokenWhitelistRepository,
+            scope=singleton,
+        )
+        binder.bind(TypeRepositoryABC, to=TypeRepository, scope=singleton)  # type: ignore[type-abstract]
+        binder.bind(UserRepositoryABC, to=UserRepository, scope=singleton)  # type: ignore[type-abstract]
