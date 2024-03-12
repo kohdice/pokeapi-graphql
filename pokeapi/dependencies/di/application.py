0d4750c1b4c0c043edce653e643b2bd0131c4854
kohdice <kohdice@gmail.com>
2024-03-12 21:59:15 +0900

    feat: Add configuration for AuthenticationService to ApplicationServiceModule

--- pokeapi/dependencies/di/application.py
+++ pokeapi/dependencies/di/application.py
@@ -1,5 +1,7 @@
 from injector import Binder, Module, singleton
 
+from pokeapi.application.services.authentication import AuthenticationService
+from pokeapi.application.services.authentication_abc import AuthenticationServiceABC
 from pokeapi.application.services.pokemon import PokemonService
 from pokeapi.application.services.pokemon_abc import PokemonServiceABC
 from pokeapi.application.services.pokemon_ability import AbilityService
@@ -11,7 +13,8 @@ from pokeapi.application.services.pokemon_type_abc import TypeServiceABC
 class ApplicationServiceModule(Module):
     """A module for providing application service-related dependencies.
 
-    This module provides the application service as a dependency to be used by other classes in the application.
+    This module provides the application service as a dependency
+    to be used by other classes in the application.
 
     """
 
@@ -22,6 +25,7 @@ class ApplicationServiceModule(Module):
             binder (Binder): The binder to configure.
 
         """
+        binder.bind(AuthenticationServiceABC, to=AuthenticationService, scope=singleton)  # type: ignore[type-abstract]
+        binder.bind(AbilityServiceABC, to=AbilityService, scope=singleton)  # type: ignore[type-abstract]
         binder.bind(PokemonServiceABC, to=PokemonService, scope=singleton)  # type: ignore[type-abstract]
         binder.bind(TypeServiceABC, to=TypeService, scope=singleton)  # type: ignore[type-abstract]
-        binder.bind(AbilityServiceABC, to=AbilityService, scope=singleton)  # type: ignore[type-abstract]
