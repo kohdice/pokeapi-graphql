acf23e00c225aa42c981c2097e8c44f67e26dff9
kohdice <kohdice@gmail.com>
2024-03-16 18:16:20 +0900

    refactor: Remove unnecessary 'type: ignore' comments

--- pokeapi/presentation/schemas/query.py
+++ pokeapi/presentation/schemas/query.py
@@ -31,7 +31,6 @@ class Query:
         description="Returns a Pokémon Ability resource by ID."
     )
 
-    # TODO: Fix test it.
     @relay.connection(relay.ListConnection[Pokemon], description="List of Pokémon.")
     def pokemons(self, info: Info) -> Iterable[Pokemon]:  # pragma: no cover
         """Returns a list of Pokémon resources.
