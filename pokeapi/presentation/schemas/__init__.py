2cf5970ea1eb2164e61b0dbec3988bbb23dd49d3
kohdice <kohdice@gmail.com>
2024-03-17 00:38:11 +0900

    feat: Add User, UserCreationResult, UserErrors

--- pokeapi/presentation/schemas/__init__.py
+++ pokeapi/presentation/schemas/__init__.py
@@ -2,9 +2,20 @@ from typing import Annotated
 
 import strawberry
 
-from pokeapi.presentation.schemas.authentication import AuthErrors, AuthResult
+from .authentication import AuthErrors, AuthResult
+from .user import User, UserCreationResult, UserErrors
 
 AUTH_PAYLOAD = Annotated[
     AuthResult | AuthErrors,
     strawberry.union("AuthPayload", description="Union of AuthResult and AuthErrors."),
+]
+USER_PAYLOAD = Annotated[
+    User | UserErrors,
+    strawberry.union("UserPayload", description="Union of User and UserErrors."),
+]
+USER_CREATION_PAYLOAD = Annotated[
+    UserCreationResult | UserErrors,
+    strawberry.union(
+        "UserCreationPayload", description="Union of UserCreationResult and UserErrors."
+    ),
 ]
