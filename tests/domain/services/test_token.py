e9f10de8cc4102b80c0d09ee4ce4e34695d56eab
kohdice <kohdice@gmail.com>
2024-03-16 22:37:45 +0900

    feat: Add extract_payload to TokenService

--- tests/domain/services/test_token.py
+++ tests/domain/services/test_token.py
@@ -5,6 +5,7 @@ from injector import Injector
 from pytest_mock import MockerFixture
 
 from pokeapi.domain.services.token import TokenService
+from pokeapi.exceptions.token import TokenVerificationError
 from tests.conftest import TEST_USER_ENTITY
 
 
@@ -35,3 +36,34 @@ class TestTokenService:
 
     def test_delete(self, service: TokenService) -> None:
         service.delete(TEST_USER_ENTITY)
+
+    def test_extract_payload(self, service: TokenService) -> None:
+        service._jwt_service.decode_token = MagicMock(  # type: ignore
+            return_value={"sub": 1, "jti": "test_jti", "username": "test_user"}
+        )
+        actual = service.extract_payload("test_token")
+
+        assert actual == {"sub": 1, "jti": "test_jti", "username": "test_user"}
+
+    def test_extract_payload_invalid_sub(self, service: TokenService) -> None:
+        service._jwt_service.decode_token = MagicMock(  # type: ignore
+            return_value={"jti": "test_jti", "username": "test_user"}
+        )
+        with pytest.raises(TokenVerificationError):
+            service.extract_payload("test_token")
+
+    def test_extract_payload_invalid_jti(self, service: TokenService) -> None:
+        service._jwt_service.decode_token = MagicMock(  # type: ignore
+            return_value={"sub": 1, "username": "test_user"}
+        )
+
+        with pytest.raises(TokenVerificationError):
+            service.extract_payload("test_token")
+
+    def test_extract_payload_invalid_username(self, service: TokenService) -> None:
+        service._jwt_service.decode_token = MagicMock(  # type: ignore
+            return_value={"sub": 1, "jti": "test_jti"}
+        )
+
+        with pytest.raises(TokenVerificationError):
+            service.extract_payload("test_token")
