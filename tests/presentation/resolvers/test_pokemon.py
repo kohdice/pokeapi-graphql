bb993f6f55bbf7d273d4aafec79721379d658fb7
kohdice <kohdice@gmail.com>
2024-03-24 14:15:07 +0900

    feat: Add get_pokemon_by_pokedex_number

---
+++ tests/presentation/resolvers/test_pokemon.py
@@ -0,0 +1,29 @@
+from unittest.mock import MagicMock
+
+from pokeapi.application.services.pokemon import PokemonService
+from pokeapi.presentation.resolvers.pokemon import get_pokemon_by_pokedex_number
+from pokeapi.presentation.schemas.pokemon import Pokemon
+from tests.conftest import TEST_POKEMON_ENTITY, MockInfo
+
+
+class TestPokemonResolver:
+    def test_pokemon(self, mock_info: MockInfo) -> None:
+        container = mock_info.context["container"]
+        service = container.get(PokemonService)
+        service.get_by_pokedex_number = MagicMock(return_value=TEST_POKEMON_ENTITY)
+        actual = get_pokemon_by_pokedex_number(1, mock_info)  # type: ignore
+
+        assert isinstance(actual, Pokemon)
+        assert actual.name == TEST_POKEMON_ENTITY.name
+        assert (
+            actual.national_pokedex_number
+            == TEST_POKEMON_ENTITY.national_pokedex_number
+        )
+
+    def test_pokemon_not_found(self, mock_info: MockInfo) -> None:
+        container = mock_info.context["container"]
+        service = container.get(PokemonService)
+        service.get_by_pokedex_number = MagicMock(return_value=None)
+        actual = get_pokemon_by_pokedex_number(0, mock_info)  # type: ignore
+
+        assert actual is None
